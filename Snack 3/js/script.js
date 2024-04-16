8f647deb9236c8475b1b8cb5c3fdfde6f4af9a45
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-16 11:40:33 +0200

    add: array cars

---
+++ Snack 3/js/script.js
@@ -0,0 +1,7 @@
+const cars = [
+    { marca: "Simone", modello: "500", alimentazione: "benzina" },
+    { marca: "Walter", modello: "Golf", alimentazione: "diesel" },
+    { marca: "Tesla", modello: "Musk", alimentazione: "elettrico" },
+    { marca: "Olga", modello: "Ypsilon", alimentazione: "gas" }
+];
+
