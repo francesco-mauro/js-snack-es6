1cfa72f50afdc961d7512e0af4d395e4a18930ce
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-16 11:48:54 +0200

    add: correction to readme and working script

--- Snack 3/README.md
+++ Snack 3/README.md
@@ -1,4 +1,5 @@
 1. Setup
     Definizione dell'array di automobili con le proprietà marca, modello e alimentazione.
-2. Filtra le automobili per tipo di alimentazione (filter)
+2. Crea un oggetto per raggruppare le automobili per tipo di alimentazione 
+    - Filtra le automobili per tipo di alimentazione (filter)
 3. Stampa i risultati con consolle log
--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -2,6 +2,15 @@ const cars = [
     { marca: "Simone", modello: "500", alimentazione: "benzina" },
     { marca: "Walter", modello: "Golf", alimentazione: "diesel" },
     { marca: "Tesla", modello: "Musk", alimentazione: "elettrico" },
-    { marca: "Olga", modello: "Ypsilon", alimentazione: "gas" }
+    { marca: "Olga", modello: "Ypsilon", alimentazione: "metano" }
 ];
 
+
+const autoRag = {
+    benzina: cars.filter(auto => auto.alimentazione === "benzina"),
+    diesel: cars.filter(auto => auto.alimentazione === "diesel"),
+    altre: cars.filter(auto => auto.alimentazione !== "benzina" && auto.alimentazione !== "diesel")
+};
+
+// Stampa 
+console.log("Auto divide per alimentazione:", autoRag, typeof autoRag);
